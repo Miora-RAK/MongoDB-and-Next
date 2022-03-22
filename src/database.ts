91292054918a348a4b59a2909aa2259a3c78767d
Miora RAKOTONDRABARY <rakotondrabarymiora@gmail.com>
2022-03-22 16:30:21 +0100

    add games data

---
+++ src/database.ts
@@ -0,0 +1,15 @@
+import { MongoClient } from "mongodb";
+const MONGODB_URI = process.env.MONGODB_URL || "";
+
+let cachedDb: MongoClient;
+
+export function getDatabase(): Promise<MongoClient> {
+  if (cachedDb) {
+    return Promise.resolve(cachedDb);
+  }
+
+  return MongoClient.connect(MONGODB_URI).then((db) => {
+    cachedDb = db;
+    return cachedDb;
+  });
+}
